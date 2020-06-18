8413cd3ce14985810a4198ebfaa9fa60315c218d
Екатерина Пожидаева <kateworks@yandex.ru>
2020-06-18 15:49:01 +0300

    fix: правка после 1-го ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,15 @@
 
-let addButton = document.querySelector('.profile__btn_action_add');
 let editButton = document.querySelector('.profile__btn_action_edit');
 let popupWindow = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__btn_action_close');
 
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__item_type_name');
+let workInput = formElement.querySelector('.popup__item_type_work');
+let name = document.querySelector('.profile__name');
+let work = document.querySelector('.profile__work');
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-    let nameInput = formElement.querySelector('.popup__item_type_name');
-    let workInput = formElement.querySelector('.popup__item_type_work');
-
-    let name = document.querySelector('.profile__name');
-    let work = document.querySelector('.profile__work');
-
-    name.textContent = nameInput.value;
-    work.textContent = workInput.value;
-
-    popupWindow.classList.remove('popup_opened');
-  }
-
-function addProfile() {
-  console.log("Add profile");
-}
-
-function editProfile() {
+function openPopup() {
   popupWindow.classList.add('popup_opened');
   let nameInput = formElement.querySelector('.popup__item_type_name');
   let workInput = formElement.querySelector('.popup__item_type_work');
@@ -39,13 +21,21 @@ function editProfile() {
   workInput.value = work.textContent;
 }
 
-function cancelChanges() {
+function closePopup() {
   popupWindow.classList.remove('popup_opened');
 }
 
-addButton.addEventListener('click', addProfile);
-editButton.addEventListener('click', editProfile);
-closeButton.addEventListener('click', cancelChanges);
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    name.textContent = nameInput.value;
+    work.textContent = workInput.value;
+    closePopup();
+  }
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
